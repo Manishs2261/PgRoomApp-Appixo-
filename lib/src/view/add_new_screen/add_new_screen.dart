4b6a5bede9e091bbf0354d84223de64d6fc3c66b
Manish sahu <122163664+Manishs2261@users.noreply.github.com>
2023-09-26 16:56:58 +0530

    add new room screen

--- lib/src/view/add_new_screen/add_new_screen.dart
+++ lib/src/view/add_new_screen/add_new_screen.dart
@@ -1,5 +1,4 @@
 import 'dart:io';
-
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:get/get_core/src/get_main.dart';
@@ -9,113 +8,198 @@ import 'package:pgroom/src/uitels/image_string/image_string.dart';
 
 class AddNewRoom extends StatefulWidget {
   const AddNewRoom({super.key});
-
   @override
   State<AddNewRoom> createState() => _AddNewRoomState();
+
+
 }
 
 class _AddNewRoomState extends State<AddNewRoom> {
+  int count = 1;
+  bool isBool = false;
 
+  List<File>? imageFileList = [];
 
   // image picker form Gallary
-  File? _selectedIMage;
+  File? _selectedCoverImage;
+  File? _selectImage;
 
   Future _pickeImageFromGallery() async {
     final image = await ImagePicker()
         .pickImage(source: ImageSource.gallery, imageQuality: 70);
     if (image == null) return;
     setState(() {
-      _selectedIMage = File(image.path);
+      // _selectedCoverImage = File(image.path);
+      imageFileList?.add(File(image.path));
     });
   }
 
+  Future _pickeCoverImageFromGallery() async {
+    final image = await ImagePicker()
+        .pickImage(source: ImageSource.gallery, imageQuality: 70);
+    if (image == null) return;
+    setState(() {
+      _selectImage = File(image.path);
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     print("rebuild add new scren");
-    print(_selectedIMage);
+
     return Scaffold(
       appBar: AppBar(
         title: Text("Room Information"),
       ),
       body: Padding(
         padding: const EdgeInsets.all(8.0),
-        child: Column(
-
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            Text("Please complete the following information exactly as it appears in your rental contract",
-            style: TextStyle(color: Colors.orange),),
-            SizedBox(height: 5,),
-            Container(
-              alignment: Alignment.center,
-              height: 200,
-              width: double.infinity,
-
-              // cover image
-              child:  Stack(
-
-                children: [
-
-                  // for initial image when your don't choose image
-                  _selectedIMage != null
-                  ? Image.file(_selectedIMage!,
-                    height: double.infinity,
-                    width: double.infinity,
-                    fit: BoxFit.cover,)
-                      : Image(image: AssetImage(roomImage),
-                    height: double.infinity,
-                    width: double.infinity,
-                    fit: BoxFit.cover,),
-
-                  // for add a image button
-
-                  _selectedIMage != null
-                  ? Text("")
-                      : Positioned(
-                    top: 60,
-                    left: 80,
-                    child: InkWell(
-                      onTap: (){
-                        _pickeImageFromGallery();
-                      },
-                      child: Container(
-                        height: 60,
-                        width: 200,
-                        alignment: Alignment.center,
-                        decoration: BoxDecoration(
-                            border: Border.all(color: Colors.white)
-                        ),
-                        child:  Text("Add cover Image",style: TextStyle(fontSize: 25,
-                            color: Colors.white,fontWeight: FontWeight.bold),),
-                      ),
-                    ),
-                  ),
-
-                 // for delete a image
-                  _selectedIMage != null
-                  ? Positioned(
-                    right: 1,
-                      child: InkWell(
-                        onTap: (){
-                          print("cut image");
-
-                          setState(() {
-                            _selectedIMage = null;
-                          });
-                        },
-                        child: CircleAvatar(
-                          radius: 14,
-                          backgroundColor: Colors.black26,
-                          child: Text("X",style: TextStyle(color: Colors.white),),),
-                      ))
-                      : Text("")
-                ],
+        child: SingleChildScrollView(
+          scrollDirection: Axis.vertical,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Text(
+                "Please complete the following information exactly as it appears in your rental contract",
+                style: TextStyle(color: Colors.orange),
+              ),
+              SizedBox(
+                height: 5,
               ),
 
+              //========stack container ============
+              Container(
+                alignment: Alignment.center,
+                height: 200,
+                width: double.infinity,
 
-            )
-          ],
+                //======== cover image=================
+                child: Stack(
+                  children: [
+                    // =====for initial image when your don't choose image============
+                    _selectedCoverImage != null
+                        ? Image.file(
+                            _selectedCoverImage!,
+                            height: double.infinity,
+                            width: double.infinity,
+                            fit: BoxFit.cover,
+                          )
+                        : Image(
+                            image: AssetImage(roomImage),
+                            height: double.infinity,
+                            width: double.infinity,
+                            fit: BoxFit.cover,
+                          ),
+
+                    // ========for add a image button=========
+
+                    _selectedCoverImage != null
+                        ? Text("")
+                        : Positioned(
+                            top: 60,
+                            left: 80,
+                            child: InkWell(
+                              onTap: () {
+                                _pickeImageFromGallery();
+                              },
+                              child: Container(
+                                height: 60,
+                                width: 200,
+                                alignment: Alignment.center,
+                                decoration: BoxDecoration(
+                                    border: Border.all(color: Colors.white)),
+                                child: Text(
+                                  "Add cover Image",
+                                  style: TextStyle(
+                                      fontSize: 25,
+                                      color: Colors.white,
+                                      fontWeight: FontWeight.bold),
+                                ),
+                              ),
+                            ),
+                          ),
+
+                    //==========for delete a image===========
+                    _selectedCoverImage != null
+                        ? Positioned(
+                            right: 1,
+                            child: InkWell(
+                              onTap: () {
+                                print("cut image");
+
+                                setState(() {
+                                  _selectedCoverImage = null;
+                                });
+                              },
+                              child: CircleAvatar(
+                                radius: 14,
+                                backgroundColor: Colors.black26,
+                                child: Text(
+                                  "X",
+                                  style: TextStyle(color: Colors.white),
+                                ),
+                              ),
+                            ))
+                        : Text("")
+                  ],
+                ),
+              ),
+
+              SizedBox(
+                height: 10,
+              ),
+
+              isBool
+                  ? Container(
+                      padding: EdgeInsets.all(10),
+                      height: 120,
+                      width: double.infinity,
+                      decoration: BoxDecoration(),
+                      child: ListView.builder(
+                          scrollDirection: Axis.horizontal,
+                          shrinkWrap: false,
+                          itemCount: imageFileList?.length,
+                          itemBuilder: (context, index) {
+                            return Container(
+                              padding: EdgeInsets.all(3),
+                              height: 120,
+                              width: 120,
+                              decoration: BoxDecoration(border: Border.all()),
+                              child: Image.file(
+                                imageFileList![index],
+                                fit: BoxFit.cover,
+                              ),
+                            );
+                          }),
+                    )
+                  : Container(
+                      height: 100,
+                      width: double.infinity,
+                      decoration: BoxDecoration(
+                        border: Border.all(color: Colors.black26),
+                      ),
+                child: Icon(Icons.image_outlined,size: 50,),
+                    ),
+
+              SizedBox(height: 10,),
+              SizedBox(
+                height: 40,
+                width: double.infinity,
+                child: ElevatedButton(
+                    onPressed: () {
+                      _pickeImageFromGallery();
+                      isBool = true;
+
+                      for (var i in imageFileList!) {
+                        print(imageFileList);
+                        print(i);
+
+                        print("image print ");
+                      }
+                    },
+                    child: Text("add")),
+              )
+            ],
+          ),
         ),
       ),
     );
