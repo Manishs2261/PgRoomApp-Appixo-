7aacc7f4859730e54d08c051ceea35192542a6ef
Manish sahu <122163664+Manishs2261@users.noreply.github.com>
2023-10-10 08:49:41 +0530

    create routes name

---
+++ lib/src/res/route_name/routes_name.dart
@@ -0,0 +1,30 @@
+class RoutesName{
+
+
+  static const String SplashScreen = '/';
+
+  static const String onboradingScreen = '/OnBoardingOne_Screen';
+
+  static const String loginScreen = '/login_screen';
+  static const String singScreen = '/sing_screen';
+  static const String emailForgetPassScreen = '/emailforgetPassword_screen';
+  static const String phoneForgetPassScreen = '/phoneforgetPassword_screen';
+  static const String otpPhoneforgetPassScreen = '/otpPhoneforgetPass_scre'
+      'en';
+  static const String otpEmailforgetPassScreen = '/otpEmailForgetPassword_scre'
+      'en';
+
+  static const String homeScreen = "/hoome_screen";
+  static const String rentDetails_screen = "/rentDetails_screen";
+
+  static const String rentFormScreen = "/rentForm_screen";
+  static const String addImageScreen = "/addImage_screen";
+  static const String chargeAndDoorTimingScreen = "/chargeAnd_doorTiming_scre"
+      "en";
+  static const String hostelAndRoomTypeScreen = "/HostelAndRoom_Type_screen";
+  static const String perimissionScreen = "/permission_screen";
+  static const String providsFacilitesScreen = "/provides_facilites_screen";
+  static const String rentDetailsFormScreen = "/rent_Details_Form_screen";
+
+
+}
