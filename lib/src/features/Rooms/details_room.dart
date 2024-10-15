00570921c278a8f9836c6fa9a9df7070a22fb5e7
Manish sahu <sanjusahu7326@gmail.com>
2024-10-15 09:17:02 +0530

    list view container

--- lib/src/features/Rooms/details_room.dart
+++ lib/src/features/Rooms/details_room.dart
@@ -1,5 +1,7 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
+
 import '../../utils/Constants/colors.dart';
 
 class DetailsRoom extends StatefulWidget {
@@ -13,6 +15,16 @@ class _DetailsRoomState extends State<DetailsRoom> {
   late ScrollController _scrollController;
   double _buttonOpacity = 1.0;
 
+  final List<String> roomImages = [
+    'https://plus.unsplash.com/premium_photo-1664474619075-644dd191935f?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8aW1hZ2V8ZW58MHx8MHx8fDA%3D',
+    'https://plus.unsplash.com/premium_photo-1664474619075-644dd191935f?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8aW1hZ2V8ZW58MHx8MHx8fDA%3D',
+    'https://plus.unsplash.com/premium_photo-1664474619075-644dd191935f?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8aW1hZ2V8ZW58MHx8MHx8fDA%3D',
+    'https://plus.unsplash.com/premium_photo-1664474619075-644dd191935f?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8aW1hZ2V8ZW58MHx8MHx8fDA%3D',
+    'https://plus.unsplash.com/premium_photo-1664474619075-644dd191935f?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8aW1hZ2V8ZW58MHx8MHx8fDA%3D',
+  ];
+
+  int currentPage = 0;
+
   @override
   void initState() {
     super.initState();
@@ -20,12 +32,14 @@ class _DetailsRoomState extends State<DetailsRoom> {
 
     // Add scroll listener to update button opacity
     _scrollController.addListener(() {
-      if (_scrollController.position.userScrollDirection == ScrollDirection.reverse) {
+      if (_scrollController.position.userScrollDirection ==
+          ScrollDirection.reverse) {
         // User is scrolling down, reduce the button's opacity
         setState(() {
           _buttonOpacity = 0.0;
         });
-      } else if (_scrollController.position.userScrollDirection == ScrollDirection.forward) {
+      } else if (_scrollController.position.userScrollDirection ==
+          ScrollDirection.forward) {
         // User is scrolling up, increase the button's opacity
         setState(() {
           _buttonOpacity = 1.0;
@@ -100,8 +114,7 @@ class _DetailsRoomState extends State<DetailsRoom> {
                     ),
                     Container(
                       margin: EdgeInsets.only(top: 12, bottom: 12),
-                      padding:
-                      EdgeInsets.symmetric(vertical: 2, horizontal: 8),
+                      padding: EdgeInsets.symmetric(vertical: 2, horizontal: 8),
                       decoration: BoxDecoration(
                           color: Colors.blueAccent.withOpacity(0.07),
                           borderRadius: BorderRadius.circular(12)),
@@ -145,7 +158,7 @@ class _DetailsRoomState extends State<DetailsRoom> {
                                 ),
                                 CircleAvatar(
                                     backgroundColor:
-                                    Colors.blueAccent.withOpacity(0.1),
+                                        Colors.blueAccent.withOpacity(0.1),
                                     radius: 8,
                                     child: Icon(
                                       Icons.close,
@@ -161,8 +174,119 @@ class _DetailsRoomState extends State<DetailsRoom> {
                   ],
                 ),
               ),
+              Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: Container(
+                  height: 500,
+                  padding: const EdgeInsets.all(16.0),
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(16.0),
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.grey.withOpacity(0.5),
+                        spreadRadius: 5,
+                        blurRadius: 7,
+                        offset: Offset(0, 3), // changes position of shadow
+                      ),
+                    ],
+                  ),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      // Image Slider
+                      Expanded(
+                        child: Stack(
+                          children: [
+                            PageView.builder(scrollDirection: Axis.horizontal,
+                              pageSnapping: true,
+
+                              itemCount: roomImages.length,
+                              onPageChanged: (int page) {
+                                setState(() {
+                                  currentPage = page;
+                                });
+                              },
+                              itemBuilder: (context, index) {
+                                return ClipRRect(
+                                  borderRadius: BorderRadius.circular(10.0),
+                                  child: CachedNetworkImage(
+                                    imageUrl: roomImages[index],
+                                    placeholder: (context, url) => Center(
+                                        child: CircularProgressIndicator()),
+                                    errorWidget: (context, url, error) =>
+                                        Icon(Icons.error),
+                                    fit: BoxFit.cover,
+                                  ),
+                                );
+                              },
+                            ),
+                            Positioned(
+                              bottom: 1,
+                              right: 1,
+                              child: Container(
+                                margin: EdgeInsets.all(8),
+                                padding: EdgeInsets.symmetric(horizontal: 8),
+                                decoration: BoxDecoration(
+                                  color: Colors.black,
+                                  borderRadius: BorderRadius.circular(50)
+                                ),
+                                child: Text(
+                                  '${currentPage + 1}/ ${roomImages.length}',
+                                  style: TextStyle(fontSize: 14,color: Colors.white),
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
 
 
+
+                      SizedBox(height: 20),
+
+                      // Room details
+                      Text(
+                        'Room Rent: \$500',
+                        style: TextStyle(
+                            fontSize: 18, fontWeight: FontWeight.bold),
+                      ),
+                      SizedBox(height: 10),
+                      Text(
+                        'Room Name: Cozy Room',
+                        style: TextStyle(fontSize: 16),
+                      ),
+                      SizedBox(height: 10),
+                      Text(
+                        'Address: 123 Main St, Springfield',
+                        style: TextStyle(fontSize: 16),
+                      ),
+                      SizedBox(height: 10),
+                      Text(
+                        'Room Type: Both (Boys & Girls)',
+                        style: TextStyle(fontSize: 16),
+                      ),
+                      SizedBox(height: 10),
+                      Text(
+                        'Update: Available for rent from October 2024',
+                        style: TextStyle(fontSize: 16),
+                      ),
+                      SizedBox(height: 20),
+
+                      // Contact Now Button
+                      Center(
+                        child: ElevatedButton(
+                          onPressed: () {
+                            // Add your contact action here
+                            print('Contact button pressed');
+                          },
+                          child: Text('Contact Now'),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
             ],
           ),
 
@@ -176,30 +300,43 @@ class _DetailsRoomState extends State<DetailsRoom> {
                 duration: Duration(milliseconds: 300),
                 opacity: _buttonOpacity,
                 child: FloatingActionButton.extended(
-                  elevation: 2,
-                  backgroundColor: AppColors.primary,
-                  onPressed: () {
-                    // Add action for the button
-                  },
-                  label: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                    children: [
-                    Column(
+                    elevation: 2,
+                    backgroundColor: AppColors.primary,
+                    onPressed: () {
+                      // Add action for the button
+                    },
+                    label: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                       children: [
-                        Icon(Icons.filter_list,color: Colors.white,),
-                        Text("Sort",style: TextStyle(color: Colors.white),)
+                        Column(
+                          children: [
+                            Icon(
+                              Icons.filter_list,
+                              color: Colors.white,
+                            ),
+                            Text(
+                              "Sort",
+                              style: TextStyle(color: Colors.white),
+                            )
+                          ],
+                        ),
+                        SizedBox(
+                          width: 16,
+                        ),
+                        Column(
+                          children: [
+                            Icon(
+                              Icons.filter_alt_outlined,
+                              color: Colors.white,
+                            ),
+                            Text(
+                              "Filter",
+                              style: TextStyle(color: Colors.white),
+                            )
+                          ],
+                        )
                       ],
-                    ),
-                    SizedBox(width: 16,),
-                    Column(
-                      children: [
-                        Icon(Icons.filter_alt_outlined,color: Colors.white,),
-                        Text("Filter",style: TextStyle(color: Colors.white),)
-                      ],
-                    )
-                  ],)
-                
-                ),
+                    )),
               ),
             ),
           ),
