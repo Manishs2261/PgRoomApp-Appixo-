21365a44c82363727d729e3ac140ea522943471c
Manish sahu <sanjusahu7326@gmail.com>
2024-11-04 22:27:06 +0530

    add button

--- lib/src/features/foods_screen_new/foods_form_screen/third_food_form.dart
+++ lib/src/features/foods_screen_new/foods_form_screen/third_food_form.dart
@@ -1,4 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:get/get_core/src/get_main.dart';
+
+import '../../../common/widgets/com_reuse_elevated_button.dart';
+import '../../../res/route_name/routes_name.dart';
 
 class ThirdFoodForm extends StatefulWidget {
   const ThirdFoodForm({super.key});
@@ -12,6 +17,28 @@ class _ThirdFoodFormState extends State<ThirdFoodForm> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text("third food form"),),
+
+      body: Column(
+        children: [
+
+          const SizedBox(
+            height: 16,
+          ),
+
+          SizedBox(height: 20),
+          ReuseElevButton(
+            onPressed: () => Get.toNamed(RoutesName.fourthFoodFormScreen),
+            title: "Save & Next",
+          ),
+
+          SizedBox(height: 20),
+          ReuseElevButton(
+            color: Colors.orange,
+            onPressed: () => Get.back(),
+            title: "Back",
+          ),
+        ],
+      ),
     );
   }
 }
