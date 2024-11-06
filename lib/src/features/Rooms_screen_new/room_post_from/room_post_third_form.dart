57db2ff3f17b183dd3ced70d88b84aa2e8330eb0
Manish sahu <sanjusahu7326@gmail.com>
2024-11-06 22:11:23 +0530

    add google map marker

--- lib/src/features/Rooms_screen_new/room_post_from/room_post_third_form.dart
+++ lib/src/features/Rooms_screen_new/room_post_from/room_post_third_form.dart
@@ -1,6 +1,9 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:get/get_core/src/get_main.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 import '../../../common/widgets/com_reuse_elevated_button.dart';
 import '../../../res/route_name/routes_name.dart';
@@ -15,6 +18,45 @@ class ThirdRoomFormScreen extends StatefulWidget {
 }
 
 class _ThirdRoomFormScreenState extends State<ThirdRoomFormScreen> {
+  final Completer<GoogleMapController> _controller =
+  Completer<GoogleMapController>();
+
+  Set<Marker> _markers = {}; // To hold the markers
+  late LatLng _lastTappedPosition; // To store last tapped position
+
+  static const CameraPosition _kGooglePlex = CameraPosition(
+    target: LatLng( 21.281420, 82.811522),
+    zoom: 12.4746,
+  );
+
+
+  void _handleTap(LatLng tappedPoint) {
+    setState(() {
+      // Clear previous markers if you only want one marker at a time
+      _markers.clear();
+
+      // Add a new marker at the tapped position
+      _markers.add(
+        Marker(
+          markerId: MarkerId(tappedPoint.toString()),
+          position: tappedPoint,
+          infoWindow: InfoWindow(
+            title: 'Selected Location',
+            snippet: 'Lat: ${tappedPoint.latitude}, Lng: ${tappedPoint.longitude}',
+          ),
+        ),
+      );
+
+      // Save the last tapped position
+      _lastTappedPosition = tappedPoint;
+    });
+
+    // Log the latitude and longitude
+    print('Tapped position: ${tappedPoint.latitude}, ${tappedPoint.longitude}');
+  }
+
+
+
   @override
   Widget build(BuildContext context) {
 
@@ -28,8 +70,27 @@ class _ThirdRoomFormScreenState extends State<ThirdRoomFormScreen> {
         backgroundColor: Colors.white,
         centerTitle: true,
       ),
+
+
       body: Column(
         children: [
+
+          Expanded(
+            child: GoogleMap(
+              mapType: MapType.hybrid,
+              initialCameraPosition: _kGooglePlex,
+              myLocationEnabled: true,
+              myLocationButtonEnabled: true,
+              compassEnabled: true,
+              markers: _markers, // Display markers on the map
+              onMapCreated: (GoogleMapController controller) {
+                _controller.complete(controller);
+              },
+              onTap: _handleTap,
+
+
+            ),
+          ),
           // Save button
           SizedBox(height: 20),
           ReuseElevButton(
@@ -46,5 +107,9 @@ class _ThirdRoomFormScreenState extends State<ThirdRoomFormScreen> {
         ],
       ),
     );
+
   }
+
+
 }
+
