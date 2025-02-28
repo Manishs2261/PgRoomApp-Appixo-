d877e2734cb9ca225bec8c320633d61bca9bceb8
Manish sahu <sanjusahu7326@gmail.com>
2025-02-28 21:02:11 +0530

    new version change

---
+++ lib/src/utils/constant/constant.dart
@@ -0,0 +1,4 @@
+const String devRoomCollection = "DevRoomCollection";
+const String devFoodCollection = "DevFoodCollection";
+const String devSellAndBuyCollection = "DevSellAndBuyCollection";
+const String devServicesCollection = "DevServicesCollection";
